e6a64216aa561e78c61636914773253118162f1b
Rafael Z. Kineippe <rafael@zamana.com.br>
2017-10-21 23:47:16 -0200

    Reviewing the deploy from Travis

--- travis/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/nash
-$(aws ecr get-login --no-include-email)
-docker tag rzamana/curriculum ${DOCKER_ECR_REPO}:${APP_VERSION}.${TRAVIS_BUILD_NUMBER}
-docker push ${DOCKER_ECR_REPO}:${APP_VERSION}.${TRAVIS_BUILD_NUMBER}
-docker tag rzamana/curriculum ${DOCKER_ECR_REPO}:latest
-docker push ${DOCKER_ECR_REPO}:latest
---
+++ travis/docker_push.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Push only if it's not a pull request
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # Push only if we're testing the master branch
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+
+    # This is needed to login on AWS and push the image on ECR
+    # Change it accordingly to your docker repo
+    pip install --user awscli
+    export PATH=$PATH:$HOME/.local/bin
+    eval $(aws ecr get-login --no-include-email)
+
+    # Build and push
+    echo "Pushing docker image"
+    docker tag rzamana/curriculum ${DOCKER_ECR_REPO}:${APP_VERSION}.${TRAVIS_BUILD_NUMBER}
+    docker push ${DOCKER_ECR_REPO}:${APP_VERSION}.${TRAVIS_BUILD_NUMBER}
+    docker tag rzamana/curriculum ${DOCKER_ECR_REPO}:latest
+    docker push ${DOCKER_ECR_REPO}:latest
+    echo "Pushed ${DOCKER_ECR_REPO}:${APP_VERSION}.${TRAVIS_BUILD_NUMBER}"
+  else
+    echo "Skipping deploy because branch is not 'master'"
+  fi
+else
+  echo "Skipping deploy because it's a pull request"
+fi
