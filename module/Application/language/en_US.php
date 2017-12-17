c72b3298d9f0737da601cad7349dd51db4bf2821
Rafael Z. Kineippe <rafael@zamana.com.br>
2017-12-17 13:11:57 -0200

    Including bio and resume bio

--- module/Application/language/en_US.php
+++ module/Application/language/en_US.php
@@ -11,4 +11,40 @@ return [
   'Education' => 'Education',
   'Skills' => 'Skills',
   'Accomplish' => 'Accomplish',
+  'Developer Curriculum' => 'Developer Curriculum',
+  'Archer Curriculum' => 'Archer Curriculum',
+  'Resume Bio' => '<p>At the end of 2003, 
+with almost 20 years old, I decided to enter 
+on the development bussines. 
+My first book "PHP & MySQL", 
+I didn\'t knew it how much this will define my future. 
+Started with the basic, missing a lot, 
+creating a some sistems with few security, 
+now everything starts with a good API to be consumed</p>',
+
+  'Full Bio' => '<p>At the end of 2003, with almost 20 years old, 
+I decided to enter on the development bussines. 
+My first book "PHP & MySQL", I didn\'t knew it how much 
+this will define my future. Started with the basic, 
+missing a lot, creating a some sistems with few security, 
+now everything starts with a good API to be consumed.</p>
+<p>Already developed small sistems and big ones, 
+learned other programming languages, and I still 
+manage to take 2 certifications (one already expired). </p>
+<p>Heard a lot about agil sistems and scrum (really helpfull), 
+using pomodoro on my free time, so I can handle to keep studying.</p>
+<p>I have my personal cluster, with 3 servers running linux (ubuntu), 
+two of which are with containered services, 
+and the third is being migrated to docker.</p>
+<p>My github has always been very slow, 
+because my first dedication is with the company I\'m working with, 
+so I always ended up with little time for personal projects 
+(and still the repositories are private).</p>
+<p>I learned 2 new languages, one for taste (English) 
+and other for work (Spanish), and all times 
+I get information on these languages and many others that 
+make me want to learn (German, Chinese, etc).</p>
+<p>I decided to do this curriculum with ZendFramework 3. 
+Why?!? Because I didn\'t know this framework and wanted
+to create something with it.</p>'
 ];
--- module/Application/language/es_ES.php
+++ module/Application/language/es_ES.php
@@ -11,4 +11,40 @@ return [
   'Education' => 'Educación',
   'Skills' => 'Habilidades',
   'Accomplish' => 'Logrado',
+  'Resume Bio' => '<p>A finales de 2003, con poco más de 19 años, 
+decidí entrar a la rama de desarrollo. 
+Mi primer libro "PHP & MySQL", 
+ni sabía cuánto se definiria mi futuro. 
+Comencé de lo básico, errando mucho, 
+creando algunos sistemas con poca seguridad, 
+ahora todo comienza con una buena API lista para ser consumida.</p>',
+
+  'Full Bio' => '
+<p>A finales de 2003, con poco más de 19 años, 
+decidí entrar a la rama de desarrollo. 
+Mi primer libro "PHP & MySQL", 
+ni sabía cuánto se definiria mi futuro. 
+Comencé de lo básico, errando mucho, 
+creando algunos sistemas con poca seguridad, 
+ahora todo comienza con una buena API lista para ser consumida.</p>
+<p>Ya he desarrollado sistemas pequeños y grandes, 
+aprendí otros lenguajes de programación, 
+y aún conseguí sacar 2 certificaciones (una de ellas ya expiró).</p>
+<p>He escuchado mucho sobre sistemas ágiles y scrum (realmente muy útil), 
+utilizando pomodoro en mis horas libres, 
+para poder dar cuenta de continuar estudiando.</p>
+<p>Tengo un clúster personal, con 3 máquinas que ejecutan linux (ubuntu), 
+siendo que 2 de ellas están con servicios en containers, 
+y la tercera está siendo migrada a docker.</p>
+<p>Mi github siempre fue muy parado, 
+porque mi primera dedicación es con la empresa que estoy trabajando, 
+entonces siempre acababa con poco tiempo para proyectos personales 
+(y aún así los repositorios son privados).</p>
+<p>Aprendí 2 nuevas lenguas, una por gusto (Inglés) 
+y otra por trabajo (Español), y en todo momento llega para mí 
+informaciones en esas lenguas y más otras que me dan ganas de 
+aprender (Alemán, Chino, etc.).</p>
+<p>Decidí hacer este currículum en ZendFramework 3. 
+¿Por qué?!? Porque todavía no conocía este framework y 
+quería mucho crear algo con él.</p>',
 ];
